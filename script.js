31301c363995e0fd487d30a77402cc7025ce4b53
ParagSenDeka <patrickstat022@gmail.com>
2024-02-29 19:48:55 +0530

    Updated

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
 
 import express from "express";
+import pg from "pg";
 const app = express();
+const db=pg.Client({
+
+});
 const port = 3000;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-
-const data = [
-    {
-        title: "Why sleeping is important",
-        time: "28-02-2024",
-        description: "Sleeping 8 hours everyday is important for men of age 18 or above, it's always recommended to maintain at least 8 hours of sleep everyday",
-        author: "Parag"
-    },
-    {
-        title: "Why eating is important",
-        time: "27-02-2024",
-        description: "eating 8 hours everyday is important for men of age 18 or above, it's always recommended to maintain at least 8 hours of food everyday",
-        author: "Parag"
-    }
-]
-
 
 
 app.get("/", (req, res) => {
@@ -28,8 +16,8 @@ app.get("/", (req, res) => {
 });
 
 app.get("/edit/:index", (req, res) => {
-    let index = req.body.index;
-    res.render("modify.ejs", { id: index });
+    let index = req.params.index;
+    res.render("modify.ejs", { index: index });
 });
 
 app.get("/new", (req, res) => {
@@ -37,7 +25,11 @@ app.get("/new", (req, res) => {
 });
 
 app.post("/submit/:id", (req, res) => {
-    console.log(data[req.params.id]);
+    // Submit the post here 
+    const index=req.params.id;
+    data[index].title=req.body.title;
+    data[index].description=req.body.description;
+    res.redirect("/");
 });
 
 app.listen(port, () => {
