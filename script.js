b93842d509c78c3ee2ecffd8cf12e868519112bf
ParagSenDeka <patrickstat022@gmail.com>
2024-02-29 21:06:53 +0530

    Updated to postgresql for data storing

--- script.js
+++ script.js
@@ -2,9 +2,26 @@
 import express from "express";
 import pg from "pg";
 const app = express();
-const db=pg.Client({
-
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "world",
+    password: "781121",
+    port: 5432,
 });
+
+let data = [];
+
+db.connect();
+db.query("SELECT * FROM blogData", (err, res) => {
+    if (err) {
+        console.error("Error executing query", err.stack);
+    } else {
+        data = res.rows;
+    }
+    db.end();
+});
+
 const port = 3000;
 
 app.use(express.static("public"));
@@ -26,9 +43,9 @@ app.get("/new", (req, res) => {
 
 app.post("/submit/:id", (req, res) => {
     // Submit the post here 
-    const index=req.params.id;
-    data[index].title=req.body.title;
-    data[index].description=req.body.description;
+    const index = req.params.id;
+    data[index].title = req.body.title;
+    data[index].description = req.body.description;
     res.redirect("/");
 });
 
