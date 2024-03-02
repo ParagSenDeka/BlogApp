89c5a11116328ab0d809552cf6e6405887292349
ParagSenDeka <patrickstat022@gmail.com>
2024-03-02 20:17:07 +0530

    Y

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ import express from "express";
 import pg from "pg";
 const app = express();
 const db = new pg.Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    user: "postgres",
+    host: "localhost",
+    database: "world",
+    password: "781121",
+    port: 5432,
 });
 
 db.connect();
