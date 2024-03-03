c6357ec55b66374ff4a1d829dcbbfd6455b10292
ParagSenDeka <patrickstat022@gmail.com>
2024-03-03 21:43:55 +0530

    m

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-// Import and configs
 import express from "express";
 import pg from "pg";
-const app = express();
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-// Creating a pg client
-const db = new pg.Client({
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Create a database pool
+const pool = new pg.Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
@@ -14,116 +16,40 @@ const db = new pg.Client({
     port: process.env.DB_PORT,
 });
 
-// Connecting the databse
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to database:', err);
-    } else {
-        console.log('Connected to database');
-    }
-});
-
-const port = 3000;
-let maxLength = 0;
-
-// Using middlewares
+// Middleware
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-// Homepage or default route
-app.get("/", (req, res) => {
-    db.query("SELECT * FROM blogData ORDER BY id", (err, result) => {
-        if (err) {
-            console.error("Error executing query", err.stack);
-            res.status(500).send("Internal Server Error");
-            return;
-        }
-        const data = result.rows;
-        maxLength = data.length;
-        res.render("index.ejs", { data: data });
-    });
+// Routes
+app.get("/", async (req, res) => {
+    try {
+        const { rows } = await pool.query("SELECT * FROM blogData ORDER BY id");
+        res.render("index.ejs", { data: rows });
+    } catch (err) {
+        console.error("Error executing query", err);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
-// Delete a blog
-app.get("/delete/:id", (req, res) => {
-    let idToDelete = req.params.id; // 
-    let deleteQuery = {
-        text: 'DELETE FROM blogData WHERE id = $1',
-        values: [idToDelete],
-    };
-
-    db.query(deleteQuery, (err, res) => {
-        if (err) {
-            console.log(err.stack);
-        }
-        else {
-            console.log("Row deleted successfully");
-        }
-    });
-    res.redirect("/");
+app.get("/delete/:id", async (req, res) => {
+    try {
+        const idToDelete = req.params.id;
+        await pool.query("DELETE FROM blogData WHERE id = $1", [idToDelete]);
+        console.log("Row deleted successfully");
+        res.redirect("/");
+    } catch (err) {
+        console.error("Error deleting row", err);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
-// Post a new blog
 app.get("/new", (req, res) => {
     res.render("modify.ejs", { maxLength: maxLength });
 });
 
-// Edit a blog
-app.get("/edit/:index", (req, res) => {
-    let index = parseInt(req.params.index);
-    let updateQuery = {
-        text: 'SELECT title, description FROM blogData WHERE id=$1',
-        values: [index]
-    };
-    db.query(updateQuery, (err, result) => {
-        if (err) {
-            console.error("Error executing query", err.stack);
-            res.status(500).send("Internal Server Error");
-            return;
-        }
-        let tempData = result.rows;
-        tempData.push(index);
-        res.render("modify.ejs", {data: tempData});
-    });
-});
+// Other routes...
 
-
-app.post("/submit/:id", (req, res) => {
-    // Submit the post here 
-    let newData = { title: req.body.title, description: req.body.description };
-    let idToUpdate = req.params.id;
-    let updateQuery = {
-        text: 'UPDATE blogData SET title = $1, description = $2,crDate=CURRENT_DATE WHERE id = $3',
-        values: [newData.title, newData.description, idToUpdate],
-    };
-    db.query(updateQuery, (err) => {
-        if (err) {
-            console.error("Error executing query", err.stack);
-        } else {
-            console.log("Row updated successfully");
-        }
-    });
-    res.redirect("/");
-});
-
-app.post("/submitNew/:id", (req, res) => {
-    let newData = { title: req.body.title, description: req.body.description, author: req.body.author };
-    let idToUpdate = maxLength + 1;
-    let addQuery = {
-        text: 'INSERT INTO blogData VALUES($1,$2,$3,CURRENT_DATE,$4)',
-        values: [newData.title, newData.description, newData.author, idToUpdate],
-    };
-    db.query(addQuery, (err) => {
-        if (err) {
-            console.error("Error executing query", err.stack);
-        } else {
-            console.log("Row added successfully");
-        }
-    });
-    res.redirect("/");
-})
-
-// Listener
+// Start server
 app.listen(port, () => {
-    console.log("Running at port " + port);
+    console.log("Server is running on port " + port);
 });
