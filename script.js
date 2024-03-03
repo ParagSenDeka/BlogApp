96fd2705d99b86e4e79197f8164b9ae8234471f2
ParagSenDeka <patrickstat022@gmail.com>
2024-03-03 23:51:56 +0530

    m

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = new pg.Pool({
-    user: DB_USER,
-    host: DB_HOST,
-    database: DB_NAME,
-    password: DB_PASSWORD,
-    port: DB_PORT,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 db.connect()
