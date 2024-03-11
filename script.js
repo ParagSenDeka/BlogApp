044197a2e892f7ea11a96095d32cb07a64992da2
Parag Deka <96935811+ParagSenDeka@users.noreply.github.com>
2024-03-11 21:16:54 +0530

    M

--- script.js
+++ script.js
@@ -13,12 +13,11 @@ app.set('view engine', 'ejs')
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+const db = new pg.Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
 });
 
 db.connect()
