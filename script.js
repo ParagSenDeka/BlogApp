510c5ee7cfd2a4322a6ddb4e012fffc5e5bbe785
Parag Deka <96935811+ParagSenDeka@users.noreply.github.com>
2024-03-26 20:24:04 +0530

    Update

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ app.use(
   })
 );
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname+"public"));
+app.use(express.static("public"));
 app.use(passport.initialize());
 app.use(passport.session());
 
