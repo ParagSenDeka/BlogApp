0c29453109a60cec45d34d64096a2b438656aa67
ParagSenDeka <patrickstat022@gmail.com>
2024-08-19 14:42:57 +0530

    N

--- script.js
+++ script.js
@@ -225,7 +225,7 @@ passport.use("local",
 passport.use("google", new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/secrets",
+  callbackURL: "/auth/google/secrets",
   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 }, async (accessToken, refreshToken, profile, cb) => {
   try{
